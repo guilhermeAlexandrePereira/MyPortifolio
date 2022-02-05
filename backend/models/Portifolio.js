0ba9e9713944a1040166cfee4418c5c5595cd5dd
guilhermeAlexandrePereira <guilhermeanp1@gmail.com>
2022-02-05 00:01:32 -0300

    criando a chave unica "slug"

--- backend/models/Portifolio.js
+++ backend/models/Portifolio.js
@@ -1,12 +1,20 @@
 const mongoose = require("mongoose");
+const slug = require("slug");
 
 const { Schema } = mongoose;
 
 const portifolioSchema = new Schema ( {
     title: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     }, 
+    slug: {
+        type: String,
+        required: true,
+        unique: true,
+        default: function(){return slug(this.title)}
+    },
     description: {
         type: String,
         required: true
